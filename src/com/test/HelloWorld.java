4757cebfb82fa9be5588403227d4c66bbe0a1d85
Hubww <594133905@qq.com>
2015-12-31 14:50:30 +0800

    测试提交

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -4,7 +4,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String ww ="hello world";
+		String ww1 = "hello"+new String(" world");
+		String ww2 = "hello world";
+		String ww3 = "hello"+" world";
+		String ww4 = "hello world"+new String("");
+		System.out.println(ww.equals(ww1));
+		System.out.println(ww==ww1);
+		System.out.println(ww==ww2);
+		System.out.println(ww==ww3);
+		System.out.println(ww==ww4);
 	}
 
 }
